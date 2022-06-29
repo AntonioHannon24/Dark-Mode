b50c5f46688af9e681ab31dd09939116eb1c4d5c
AntonioHannon24 <107659611+AntonioHannon24@users.noreply.github.com>
2022-06-29 01:34:36 -0300

    confirmar

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const changeThemeBtn = document.querySelector("#change-theme");
+
+//Toggle dark mode
+function toggleDarkMode(){
+    document.body.classList.toggle("dark");
+}
+
+// Load Light or Dark mode
+function loadTheme(){
+    const darkMode = localStorage.getItem("dark");
+    if(darkMode){
+        toggleDarkMode();
+    } 
+}
+
+loadTheme();
+changeThemeBtn.addEventListener("change",function(){
+    // chamando a troca de tema
+    toggleDarkMode();
+    //salvando sua ultima preferencia
+    localStorage.removeItem("dark")
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("dark",1);
+    }
+    console.log(localStorage.getItem('dark'));
+});
